a45a8fc03a357b6b5e6833c68173af2303c8fc3e
不才 <123969432+241793@users.noreply.github.com>
2026-01-07 00:15:14 +0800

    Update qinglong_plugin.py

--- plugins/qinglong_plugin.py
+++ plugins/qinglong_plugin.py
@@ -7,9 +7,9 @@ from containers.qinglong import QinglongContainer
 from utils.logger import get_logger
 import asyncio
 from middleware.middleware import Middleware
-__description__ = "通过聊天指令与青龙面板交互"
+__description__ = "通过聊天指令与青龙面板交互，一个示例插件"
 __version__ = "1.0.0"
-__author__ = "YourName"
+__author__ = "bucai"
 
 logger = get_logger(__name__)
 
@@ -104,39 +104,6 @@ async def handle_ql_command(message, middleware):
             "to_user_id": message["user_id"]
         }
 
-async def cs(message, middleware):
-    if not await middleware.is_admin(message["user_id"]):
-        return {"content": "您没有权限执行此操作。", "to_user_id": message["user_id"]}
-    # content = message.get('content', '').strip()
-    # parts = content.split()
-    # if len(parts) < 3 or parts[0].lower() != 'ql' or parts[1].lower() != 'run':
-    #     return
-    # task_identifier = parts[2]
-    # container_name = parts[3] if len(parts) > 3 else None
-    containers_config = await middleware.bucket_manager.get("system", "containers", {})
-    if not containers_config:
-        return {"content": "尚未配置任何青龙容器。", "to_user_id": message["user_id"]}
-    for name, config in containers_config.items():
-        if config.get('enabled'):
-            target_container = config
-            target_container['name'] = name
-            break
-    client = QinglongClient(
-        url=target_container['url'],
-        client_id=target_container['client_id'],
-        client_secret=target_container['client_secret']
-    )
-    d = [{
-        "name": "cs666",
-        "value": "66666666666",
-        "remarks":""
-    }]
-    await asyncio.get_running_loop().run_in_executor(None, lambda: client.add_envs(d))
-    return {
-        "content": f"添加cs666'...",
-        "to_user_id": message["user_id"]
-    }
-
 
 # 定义规则
 rules = [
@@ -144,13 +111,6 @@ rules = [
         "name": "ql_command_rule",
         "pattern": r"^ql\s+run\s+.*",
         "handler": handle_ql_command,
-        "priority": 100,
-        "description": "处理青龙面板运行任务的指令"
-    },
-    {
-        "name": "ql_command",
-        "pattern": r"^ql\s+add\s+.*",
-        "handler": cs,
         "priority": 100,
         "description": "处理青龙面板运行任务的指令"
     }
